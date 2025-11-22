5cd4e41a6c1ddae9484d5adbbd4272dca01908f2
Seth Morgan <seth@sethmorgan.io>
2025-11-22 07:27:59 -0800

    hide secure variables

--- play_by_play_mlr_and_milr.py
+++ play_by_play_mlr_and_milr.py
@@ -2,40 +2,27 @@ import praw
 from dhooks import Webhook
 import datetime
 import time
+from os import getenv
 
-#  Enter the search terms for your MLR and MiLR team in the quotes, for example 'Oakland A' or 'Philadelphia B'.
-search_term_mlr = ''
-search_term_milr = ''
+subreddit = getenv('SUBREDDIT')
+search_term = 'Williamsport Woodcocks'
+webhook = Webhook(getenv('WEBHOOK'))
+reddit_client_id = getenv('REDDIT_CLIENT_ID')
+reddit_client_secret = getenv('REDDIT_CLIENT_SECRET')
 
-#  Create a webhook in your server by going to Server Settings > Integrations > Webhooks, and create a new Webhook. Set
-#  the name and channel, and add an icon if you'd like. Click the Copy Webhook URL button and paste it below inside the 
-#  quotes.
-webhook_MLR = Webhook('')
-webhook_MiLR = Webhook('')
-
-#  To generate a client ID and secret, go here: https://www.reddit.com/prefs/apps scroll all the way to the bottom, and
-#  hit the create an app button. Enter something for name and redirect URL, and make sure the script radio button is
-#  selected. Hit the create app button, and then paste the client ID and secret below. The user_agent string literally 
-#  just needs to have some text in it, does not matter what. 
 reddit = praw.Reddit(
-    client_id='',
-    client_secret='',
-    user_agent=''
+    client_id = reddit_client_id,
+    client_secret = reddit_client_secret,
+    user_agent = 'sethurax'
 )
 
-# Don't change anything after here.
-
-
 def parse_comments():
-    for comment in reddit.subreddit('fakebaseball').stream.comments(skip_existing=True):
+    for comment in reddit.subreddit(subreddit).stream.comments(skip_existing=True):
         update = '**/u/%s on [%s](<https://www.reddit.com%s>)**' % (comment.author, comment.link_title, comment.permalink)
         update += '```%s```' % comment.body
         update += '*Created at %s*' % (datetime.datetime.fromtimestamp(comment.created))
-        if search_term_mlr.lower() in comment.link_title.lower():
-            webhook_MLR.send(update)
-        elif search_term_milr.lower() in comment.link_title.lower():
-            webhook_MiLR.send(update)
-
+        if search_term.lower() in comment.link_title.lower():
+            webhook.send(update)
 
 while True:
     try:
